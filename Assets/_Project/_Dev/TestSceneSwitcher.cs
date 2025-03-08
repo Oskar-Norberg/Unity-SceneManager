9e80fb1be6d5429a520c4685de75172824ef427c
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-08 15:47:28 +0100

    REMOVE: [Script] Singleton

--- Assets/_Project/_Dev/TestSceneSwitcher.cs
+++ Assets/_Project/_Dev/TestSceneSwitcher.cs
@@ -1,28 +1,32 @@
 using ringo.SceneSystem;
-using ringo.SceneSystem.Helpers;
 using UnityEngine;
 
-public class TestSceneSwitcher : PersistentSingleton<TestSceneSwitcher>
+public class TestSceneSwitcher : MonoBehaviour
 {
     [SerializeField] private SceneGroup[] sceneGroups;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            SceneManager.Instance.LoadScene(sceneGroups[0]);
+            SceneManager.LoadScene(sceneGroups[0]);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            SceneManager.Instance.LoadScene(sceneGroups[1]);
+            SceneManager.LoadScene(sceneGroups[1]);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            SceneManager.Instance.LoadScene(sceneGroups[2]);
+            SceneManager.LoadScene(sceneGroups[2]);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            SceneManager.Instance.LoadScene(sceneGroups[3]);
+            SceneManager.LoadScene(sceneGroups[3]);
         }
     }
 }
