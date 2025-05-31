5c2d850db5e2b45e031d5274ff46039975e91ddb
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-05-31 16:11:25 +0200

    EDIT: [Script] Make SceneGroup Serializable

--- Runtime/SceneGroup.cs
+++ Runtime/SceneGroup.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace ringo.SceneSystem
 {
+    [Serializable]
     [CreateAssetMenu(fileName = "SceneGroup", menuName = "SceneManagement/SceneGroup")]
     public class SceneGroup : ScriptableObject
     {
